fda4811f01c68f28e82a52a39a303e00604d6e10
eknathpokale31 <eknathpokale@gmail.com>
2024-10-12 00:33:08 +0530

    Add files via upload

---
+++ ContactsApi/ContactsApi/Controllers/ContactsController.cs
@@ -0,0 +1,69 @@
+﻿using ContactsAPI.Models;
+using ContactsAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ContactRepository _repository;
+
+        public ContactsController()
+        {
+            _repository = new ContactRepository();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Contact>> Get()
+        {
+            return Ok(_repository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Contact> Get(int id)
+        {
+            var contact = _repository.GetById(id);
+            if (contact == null)
+                return NotFound();
+            return Ok(contact);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Contact contact)
+        {
+            if (contact == null)
+                return BadRequest();
+
+            _repository.Add(contact);
+            return CreatedAtAction(nameof(Get), new { id = contact.Id }, contact);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Contact contact)
+        {
+            if (contact == null || contact.Id != id)
+                return BadRequest();
+
+            var existingContact = _repository.GetById(id);
+            if (existingContact == null)
+                return NotFound();
+
+            _repository.Update(contact);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var existingContact = _repository.GetById(id);
+            if (existingContact == null)
+                return NotFound();
+
+            _repository.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ ContactsApi/ContactsApi/Repositories/ContactRepository.cs
@@ -0,0 +1,58 @@
+﻿using ContactsAPI.Models;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace ContactsAPI.Repositories
+{
+    public class ContactRepository
+    {
+        private readonly string _filePath = "contacts.json";
+
+        public List<Contact> GetAll()
+        {
+            if (!File.Exists(_filePath))
+                return new List<Contact>();
+
+            var jsonData = File.ReadAllText(_filePath);
+            return JsonConvert.DeserializeObject<List<Contact>>(jsonData) ?? new List<Contact>();
+        }
+
+        public Contact GetById(int id)
+        {
+            var contacts = GetAll();
+            return contacts.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Add(Contact contact)
+        {
+            var contacts = GetAll();
+            contact.Id = contacts.Count > 0 ? contacts.Max(c => c.Id) + 1 : 1;
+            contacts.Add(contact);
+            File.WriteAllText(_filePath, JsonConvert.SerializeObject(contacts, Newtonsoft.Json.Formatting.Indented));
+        }
+
+        public void Update(Contact contact)
+        {
+            var contacts = GetAll();
+            var existingContact = contacts.FirstOrDefault(c => c.Id == contact.Id);
+            if (existingContact != null)
+            {
+                existingContact.FirstName = contact.FirstName;
+                existingContact.LastName = contact.LastName;
+                existingContact.Email = contact.Email;
+                File.WriteAllText(_filePath, JsonConvert.SerializeObject(contacts, Newtonsoft.Json.Formatting.Indented));
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var contacts = GetAll();
+            var contactToRemove = contacts.FirstOrDefault(c => c.Id == id);
+            if (contactToRemove != null)
+            {
+                contacts.Remove(contactToRemove);
+                File.WriteAllText(_filePath, JsonConvert.SerializeObject(contacts, Newtonsoft.Json.Formatting.Indented));
+            }
+        }
+    }
+}
